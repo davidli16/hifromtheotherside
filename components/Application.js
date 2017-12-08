d3952be84db7156e4462b59c42bc49cf1d3c085e
David Li <davidli@cs.stanford.edu>
2017-12-08 01:27:41 -0800

    Initial setup
    * React
    * Babel
    * Parcel
    * PostCSS
    * autoprefixer
    * prettier
    * ESLint
    * stylelint

---
+++ components/Application.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Application extends React.Component {
+  render() {
+    return (
+      <div>
+        <header />
+        <main />
+        <footer />
+      </div>
+    );
+  }
+}
+
+export default Application;
